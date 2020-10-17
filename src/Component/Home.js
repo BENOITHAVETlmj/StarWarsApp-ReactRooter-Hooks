0257c30706a0ab528831c4e314d60ada70833d26
BENOIT-HAVET LEE <myungjae84@gmail.com>
2020-10-17 19:46:28 +0200

    insta thumbnails and redirect

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,10 +3,14 @@ import "../index.css";
 
 function Home() {
   const [payloadInsta, setPayloadInsta] = useState([]);
+  const [thumbnails, setThumbnails] = useState([
+    { node: { dimensions: {}, display_url: {} } },
+  ]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchItems = async () => {
@@ -15,10 +19,9 @@ function Home() {
 
     const payloadInsta = await data.json();
     setPayloadInsta(payloadInsta.graphql.user);
+    setThumbnails(payloadInsta.graphql.user.edge_owner_to_timeline_media.edges);
     setIsLoading(false);
   };
-
-  console.log(payloadInsta);
 
   return (
     <div>
@@ -26,8 +29,30 @@ function Home() {
         <>...loading</>
       ) : (
         <>
-          <h2>{payloadInsta.biography}</h2>
-          <img src={payloadInsta.profile_pic_url} alt="profil_pic" />
+          <div className="home-header">
+            <img
+              className="home-header-img"
+              src={payloadInsta.profile_pic_url}
+              alt="profil_pic"
+            />
+            <h2>{payloadInsta.biography}</h2>
+          </div>
+          {thumbnails?.map((t, index) => (
+            <a
+              href="https://www.instagram.com/starwars/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="home-thumbnail-img"
+                key={index}
+                src={t.node.display_url}
+                alt={t.display_url}
+                width={300}
+                height={350}
+              />
+            </a>
+          ))}
         </>
       )}
     </div>
