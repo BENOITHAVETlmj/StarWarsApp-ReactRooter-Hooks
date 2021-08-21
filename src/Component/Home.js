4418eaac7698c648a474809020af185faa15fc44
BENOIT-HAVET LEE <myungjae84@gmail.com>
2021-08-21 14:56:13 +0200

    add video with react player

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,73 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../index.css";
-import { css } from "@emotion/core";
-import ClipLoader from "react-spinners/ClipLoader";
-
-const override = css`
-  display: block;
-  margin: 20px auto;
-`;
+import ReactPlayer from "react-player/youtube";
 
 function Home() {
-  const [payloadInsta, setPayloadInsta] = useState([]);
-  const [thumbnails, setThumbnails] = useState([
-    { node: { dimensions: {}, display_url: {} } },
-  ]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    fetchItems();
-  }, []);
-
-  const fetchItems = async () => {
-    setLoading(true);
-    const data = await fetch("https://www.instagram.com/starwars/?__a=1");
-    const payloadInsta = await data.json();
-    console.log(payloadInsta);
-    setLoading(false);
-    setPayloadInsta(payloadInsta.graphql.user);
-    setThumbnails(payloadInsta.graphql.user.edge_owner_to_timeline_media.edges);
-  };
-
+  const video1 = "https://www.youtube.com/watch?v=E3-CpzZJl8w";
   return (
-    <div>
-      {loading ? (
-        <ClipLoader css={override} size={350} color={"yellow"} />
-      ) : (
-        <>
-          <div className="home-header">
-            <img
-              className="home-header-img"
-              src={payloadInsta.profile_pic_url}
-              alt="profil_pic"
-            />
-            <a
-              style={{ cursor: "pointer", color: "yellow" }}
-              href="https://www.instagram.com/starwars/?hl=en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>{payloadInsta.biography}</h2>
-            </a>
-          </div>
-          {thumbnails?.map((t, index) => (
-            <a
-              key={index}
-              href={t.node.display_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="home-thumbnail-img"
-                src={t.node.display_url}
-                alt={t.display_url}
-                width={300}
-                height={350}
-              />
-            </a>
-          ))}
-        </>
-      )}
+    <div
+      style={{
+        diplay: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <ReactPlayer
+        url={video1}
+        height={400}
+        width={600}
+        style={{
+          marginRight: "auto",
+          marginLeft: "auto",
+          marginTop: "100px",
+        }}
+        playing="true"
+        controls="true"
+      />
+      <p>
+        Si votre navigateur ne prend pas en charge les vidéos HTML5. Voici{" "}
+        <a href={video1} rel="noopener noreferrer">
+          un lien pour télécharger la vidéo
+        </a>
+        .
+      </p>
     </div>
   );
 }
